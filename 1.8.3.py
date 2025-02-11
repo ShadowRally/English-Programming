1a640151845c8ffb94b13dac1ba82011aeaf19b0
SebbeG07 <sebastianwolff07@gmail.com>
2025-02-11 08:45:04 +0100

    School

---
+++ 1.8.3.py
@@ -0,0 +1,7 @@
+import math
+math1=int(input("Write a number: "))
+math2=int(input("Write another number: "))
+def my_function(math1,math2):
+  return (math1+math2)
+
+print (my_function(math1,math2))
